c3a36b1cad0a3e41136efe109224d995b7dd3826
ShaankariVoruganti <67142384+ShaankariVoruganti@users.noreply.github.com>
2023-08-15 16:34:31 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,49 @@
+/*to create service principal through azure cli : az ad sp create-for-rbac --role="Contributor" --scopes="/subscriptions/subscription-id"*/
+terraform {
+
+  required_providers {
+    azuread = "~> 2.9.0"
+    random  = "~> 3.1"
+    azurerm = "~> 3.0.0"
+
+  }
+}
+provider "azurerm" {
+     features {
+
+     }
+client_id= "e7872244-12cf-4535-8884-dbcc07e91e9e"
+client_secret= "5O48Q~jrBLbWhnZ4RwtAc4mzwI7fQys5BLo6Mali"
+tenant_id= "a81cf97c-ef59-4c28-a9a5-15de98fce51b"
+subscription_id="4f9b03a1-6c02-4fb5-9505-d3e99e26e2ff"
+}
+
+resource "azurerm_resource_group" "resource-group" {
+  name     = "MY-RG-test"
+  location = "West Europe"
+}
+resource "azurerm_kubernetes_cluster" "default" {
+  name                = "aks90237"
+  location            = azurerm_resource_group.resource-group.location
+  resource_group_name = azurerm_resource_group.resource-group.name
+  dns_prefix          = "aksdnspre90237"
+
+  default_node_pool {
+    name            = "default"
+    node_count      = 2
+    vm_size         = "Standard_B2s"
+    os_disk_size_gb = 30
+  }
+
+  service_principal {
+ client_id     = "e7872244-12cf-4535-8884-dbcc07e91e9e"
+client_secret = "5O48Q~jrBLbWhnZ4RwtAc4mzwI7fQys5BLo6Mali"
+
+  }
+
+  role_based_access_control_enabled = true
+
+  tags = {
+    environment = "Demo"
+  }
+}
